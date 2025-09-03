3925d09cd74e0a60d8e8f08afeedf3dfa20af3f3
juliMAB <julian6aguirre@hotmail.com>
2025-09-03 16:06:35 -0300

    Add función de expresión.

---
+++ 1_funcion.js
@@ -0,0 +1,9 @@
+// Requerimiento 1: Transformar declaración a función de expresión
+// Original:
+// function example(a, b, c){
+//   return a + b + c
+// }
+
+const example = function (a, b, c) {
+  return a + b + c;
+};
